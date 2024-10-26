0ce3ca52a23b69db5030525b7c6eac6ba3d25851
Mathilde-Def <84182529+Mathilde-Def@users.noreply.github.com>
2024-10-26 08:27:41 +0000

    Add files via upload

---
+++ R scripts/codes/setup.R
@@ -0,0 +1,64 @@
+################################################################################
+#                ATTACH LIBRARY; CREATE DIRRECTORY, SET PATHS 
+#                           Mathilde Defourneaux
+#                               Fev 07, 2024
+################################################################################
+
+# Chapter 2 - Assessing Icelandic tundra herbivore faecal nutrient faecal 
+# nutrient contribution accross the growing season  
+# contact Mathilde Defourneaux (mathilde@lbhi.is) for more information
+
+# for libraries that are not installed, run install.packages("[packagename]")
+
+# Function to install and load libraries
+install_load_library <- function(libs) {
+  for (lib in libs) {
+    if (!requireNamespace(lib, quietly = TRUE)) {
+      install.packages(lib, dependencies = TRUE)
+    }
+    library(lib, character.only = TRUE)
+  }
+}
+
+# List of libraries
+
+# to install the master branch version of spectratrait 
+# make sure that all packages are up to date at this stage
+# devtools::install_github(repo = "TESTgroup-BNL/spectratrait", dependencies=TRUE)
+
+libs <- c("tidyverse", # data wrangling, manipulation
+          "data.table", # saving and loading data
+          "broom", # summarise models
+          "lubridate", # handle dates
+          "spectratrait", # spectral data manipulation -derivatives, smoothing,...-
+          "prospectr", 
+          "ggplot2", # data plotting and visualization
+          "ggimage", # support library for plotting
+          "gridExtra", # support library for plotting
+          "ggpubr", # support library for plotting
+          "ggrepel", # support library for plotting
+          "png", # support library for plotting
+          "grid", # support library for plotting
+          "plotrix", # Specialized plots and plotting accessories
+          "pls", # partial least regression square analysis
+          "mgcv", # NMDS
+          "vegan", # NMDS
+          "MASS", #NMDS
+          "emmeans", # multiple pairwise comparison
+          "lme4") # ANOVA
+
+# Install and load libraries
+install_load_library(libs)
+
+# to install the master branch version of spectratrait 
+# make sure that all packages are up to date at this stage
+# devtools::install_github(repo = "TESTgroup-BNL/spectratrait", dependencies=TRUE)
+
+# Script options
+pls::pls.options(plsralg = "oscorespls")
+pls::pls.options("plsralg")
+
+# create folders in working directories, set paths
+dir.create(paste0(getwd(),"/figures/nirs_data"))
+dir.create(paste0(getwd(),"/figures/pls_model"))
+dir.create(paste0(getwd(),"/figures/tables"))
